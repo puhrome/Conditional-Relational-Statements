c02307f8a1fbcb59c187e1315b20d56fb6f92ed3
puhrome <phirom.yim@fullsail.edu>
2014-04-14 12:45:07 -0400

    Relational Statement
    Using WebStorm to create relational statement if a value is greater or
    less to make statement true or false.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Relational Statement Created by puhrome on 4/14/14.
+ */
+
+var kidHeight = 50;
+var minHeight = 48;
+
+//if the child is tall enough, print to the console, you can ride
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight) {
+    //code performed if condition is true
+    //condition goes inside of ( )
+    //code block goes inside of { }
+    console.log("You can ride the coaster");
+}
